74e78d0621505762dd4eed91481a46663cdb055e
Julen <jgoniji20dw@ikzubirimanteo.com>
2021-09-29 11:42:52 +0200

    Execises finished

---
+++ tienda.php
@@ -0,0 +1,22 @@
+<?php
+
+$total_compra = 81;
+$tipo_compra = "ropa";
+
+if ($total_compra >= 19) {
+    if ($tipo_compra == "mascotas") {
+        echo "no se realizan envios";
+    }elseif ($total_compra >= 19 && $total_compra <= 40) {
+        echo "El envio cuesta 9€";
+    }elseif ($total_compra > 80){
+        echo "El envio es gratis";
+    }
+}
+if ($tipo_compra == "mascotas") {
+    $total_compra = $total_compra + ($total_compra / 10);
+}else{
+    $total_compra = $total_compra + (($total_compra * 100)/21);
+}
+echo "</br>";
+echo "El precio final es: ". round($total_compra, 2)."€";
+?>
